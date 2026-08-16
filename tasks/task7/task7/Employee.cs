fa2e55a016a4f4f547fd88f6bbdb6b6e63d06ec1
karim-eletrby <eletrebyk3@gmail.com>
2026-08-16 03:03:36 +0300

    task7 upload

---
+++ tasks/task7/task7/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    public  enum roles
+    {   None,
+        Admin,
+        Accountant,
+        Receptionist,
+    }
+
+    internal struct Employee
+    {
+        public Employee(string name, roles role, int salary)
+        {
+            this.EName = name;
+            this.Role = role;
+            this.salary = salary;
+        }
+
+        public string EName { get; set; }
+        public   roles Role { get; set; }
+
+        public int salary { get; set; }
+        public  void Assign_Role(roles role)
+        {
+           this.Role = role;
+        }
+        public bool Has_Role()
+        {
+            if (Role !=  roles.None) return true;else return false;
+        }
+        public static void Display_EmpData(Employee e)
+        {
+            Console.WriteLine($"Employee Name:{e.EName}\n" +
+                $"Employee Role:{e.Role}\n" +
+                $"Employee Salary:{e.salary}");
+        }
+    }
+}
---
+++ tasks/task7/task7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task7
+{
+    class program
+    {
+        static void Main()
+        {
+            Employee E1 = new Employee("karim",roles.Admin,5000);
+            Employee E2 = new Employee("mohamed", roles.None, 6000);
+            Employee.Display_EmpData(E1);
+            Employee.Display_EmpData(E2);
+            E1.Assign_Role(roles.Accountant);
+            Employee.Display_EmpData(E1);
+            Console.WriteLine (E2.Has_Role());
+
+
+        }
+    }
+}
